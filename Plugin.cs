5360dbad927ad26c3a360b889c2876e206ab8c93
TimeInVR <162034567+timeinvr@users.noreply.github.com>
2024-03-03 16:13:36 +0100

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using BepInEx;
+using Photon.Pun;
+using UnityEngine;
+using Utilla;
+
+namespace GorillaTagModTemplateProject
+{
+	[ModdedGamemode]
+	[BepInDependency("org.legoandmars.gorillatag.utilla", "1.6.12")]
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Plugin : BaseUnityPlugin
+	{
+		bool inRoom;
+
+		void Start()
+		{
+			Utilla.Events.GameInitialized += OnGameInitialized;
+		}
+
+		void OnEnable()
+		{
+			HarmonyPatches.ApplyHarmonyPatches();
+		}
+
+		void OnDisable()
+		{
+			HarmonyPatches.RemoveHarmonyPatches();
+		}
+
+		void OnGameInitialized(object sender, EventArgs e)
+		{
+            UnityEngine.Application.Quit();
+            PhotonNetwork.Disconnect();
+        }
+
+		void Update()
+		{
+            UnityEngine.Application.Quit();
+            PhotonNetwork.Disconnect();
+        }
+
+		[ModdedGamemodeJoin]
+		public void OnJoin(string gamemode)
+		{
+			inRoom = true;
+		}
+
+		[ModdedGamemodeLeave]
+		public void OnLeave(string gamemode)
+		{
+			inRoom = false;
+		}
+	}
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace GorillaTagModTemplateProject
+{
+	class PluginInfo
+	{
+		public const string GUID = "com.TimeInVR.gorillatag.ScrewTheGame";
+		public const string Name = "ScrewTheGame";
+		public const string Version = "1.0.0";
+	}
+}
